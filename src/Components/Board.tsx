c8264b84a65109c34a355b9d9330d5ce0d024282
yousifshamoun <yousif2002@berkeley.edu>
2023-01-02 21:13:04 -0800

    dark mode

--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -1,71 +1,87 @@
 import React, { ReactElement } from "react";
-import { RootState } from "../reduxStore/reducers/index"
-import { connect, ConnectedProps } from 'react-redux';
-import {Point} from "../reduxStore/reducers/global"
+import { RootState } from "../reduxStore/reducers/index";
+import { connect, ConnectedProps } from "react-redux";
+import { Point } from "../reduxStore/reducers/global";
 const mapStateToProps = (state: RootState) => ({
     global: state.global,
-}
-)
+});
 const mapDispatchToProps = {
     addPoint: (point: Point) => ({
         type: "ADD_POINT",
-        payload: point
+        payload: point,
     }),
     incrementMaxId: () => ({
         type: "INCREMENT_MAX_ID",
-    })
-}
-const connector = connect(mapStateToProps, mapDispatchToProps)
+    }),
+};
+const connector = connect(mapStateToProps, mapDispatchToProps);
 
 type PropsFromRedux = ConnectedProps<typeof connector>;
 type IBoardProps = PropsFromRedux & {
-    component?: ReactElement
-}
+    component?: ReactElement;
+};
 type BoardProps = {
     bg: React.RefObject<SVGSVGElement>;
 };
 
 class Board extends React.Component<IBoardProps, BoardProps> {
     constructor(props: IBoardProps) {
-        super(props)
+        super(props);
         this.state = {
-            bg: React.createRef()
-        }
+            bg: React.createRef(),
+        };
     }
     handleClick = (event: React.MouseEvent<SVGSVGElement>) => {
         const target = event.target as SVGSVGElement;
         const X = event.clientX - target.getBoundingClientRect().left;
         const Y = event.clientY - target.getBoundingClientRect().top;
 
-        if (this.props.global.started || window.innerWidth * .97 <= X || window.innerHeight * 0.87 <= Y || 20 >= X || 20 >= Y || !X || !Y ) {
-            return
+        if (
+            this.props.global.started ||
+            window.innerWidth * 0.97 <= X ||
+            window.innerHeight * 0.87 <= Y ||
+            20 >= X ||
+            20 >= Y ||
+            !X ||
+            !Y
+        ) {
+            return;
         }
-        this.props.addPoint({id: this.props.global.max_id, coordinates: [X, Y]})
-        this.props.incrementMaxId()
-    }
+        this.props.addPoint({
+            id: this.props.global.max_id,
+            coordinates: [X, Y],
+        });
+        this.props.incrementMaxId();
+    };
     public render() {
         return (
             <div>
-                 <svg
-                    fill="current" 
+                <svg
+                    fill="current"
                     width="100%"
                     height="90vh"
                     ref={this.state.bg}
-                    onClick={this.handleClick}>
-                        <defs>
-                            <marker id="markerArrow" markerWidth="20" markerHeight="20" refX="10" refY="6" orient="auto">
-                                <path d="M2,2 L2,11 L10,6 L2,2" fill="red" />
-                            </marker>
-                        </defs>
-                        {this.props.global.points.map((o: Point) => (
-                        <g
-                            key={o.id}
+                    onClick={this.handleClick}
+                >
+                    <defs>
+                        <marker
+                            id="markerArrow"
+                            markerWidth="20"
+                            markerHeight="20"
+                            refX="10"
+                            refY="6"
+                            orient="auto"
                         >
+                            <path d="M2,2 L2,11 L10,6 L2,2" fill="red" />
+                        </marker>
+                    </defs>
+                    {this.props.global.points.map((o: Point) => (
+                        <g key={o.id}>
                             <circle
                                 cx={o.coordinates[0]}
                                 cy={o.coordinates[1]}
                                 r={9}
-                                style={{ fill: 'red' }}
+                                style={{ fill: "red" }}
                                 stroke="black"
                                 strokeWidth="1"
                             />
@@ -73,10 +89,9 @@ class Board extends React.Component<IBoardProps, BoardProps> {
                     ))}
                     {this.props.global.render_primary}
                     {this.props.global.render_secondary}
-                    </svg>
+                </svg>
             </div>
-        )
-
+        );
     }
 }
-export default connector(Board)
+export default connector(Board);
--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,145 +1,219 @@
-import React from "react"
-import {AppBar, Button, Toolbar, Grid, Box, Typography} from '@mui/material';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import kMeans from "../Algorithms/Clustering/kMeans"
+import React from "react";
+import {
+    AppBar,
+    Button,
+    Toolbar,
+    Grid,
+    Box,
+    Typography,
+    SvgIcon,
+} from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import kMeans from "../Algorithms/Clustering/kMeans";
 import meanShift from "../Algorithms/Clustering/meanShift";
 import RandomMenu from "./RandomMenu";
 import AlgorithmMenu from "./AlgorithmMenu";
-import SpeedController from './SpeedController';
-import { RootState } from "../reduxStore/reducers/index"
-import { connect, ConnectedProps } from 'react-redux';
+import SpeedController from "./SpeedController";
+import { RootState } from "../reduxStore/reducers/index";
+import { connect, ConnectedProps } from "react-redux";
 import CentroidSlider from "./CentroidSlider";
 import WindowSizeSlider from "./WindowSizeSlider";
 import dbScan from "../Algorithms/Clustering/dbScan";
 import EpsilonSlider from "./EpsilonSlider";
 import MinPointsSlider from "./MinPointsSlider";
-import DarkModeButton from "./DarkModeButton";
-
+import DarkModeButton from "./DarkModeButton/DarkModeButton";
 const theme = createTheme({
     palette: {
-      primary: {
-        main: '#34495E',
-      },
-      secondary : {
-        main: "#1ABC9C",
-        contrastText: '#fff',
-      }
+        primary: {
+            main: "#34495E",
+        },
+        secondary: {
+            main: "#1ABC9C",
+            contrastText: "#fff",
+        },
     },
-  });
+});
 const mapState = (state: RootState) => ({
-    global: state.global
-})
+    global: state.global,
+});
 const mapDispatch = {
     pause: () => ({
-        type: "PAUSE"
+        type: "PAUSE",
     }),
     resume: () => ({
-        type: "RESUME"
+        type: "RESUME",
     }),
     reset: () => ({
-        type: "RESET"
-    })
-}
-const connector = connect(mapState, mapDispatch)
-type PropsFromRedux = ConnectedProps<typeof connector>
+        type: "RESET",
+    }),
+};
+const connector = connect(mapState, mapDispatch);
+type PropsFromRedux = ConnectedProps<typeof connector>;
 //Variables
-type NavBarState = {noAlgo: boolean}
+type NavBarState = { noAlgo: boolean };
 class NavBar extends React.Component<PropsFromRedux, NavBarState> {
     constructor(props: PropsFromRedux) {
         super(props);
         this.state = {
-            noAlgo: true
+            noAlgo: true,
         };
-      }
+    }
     handleStart = () => {
         switch (this.props.global.algorithm) {
             case "kmeans":
-                kMeans()
+                kMeans();
                 this.setState({
-                    noAlgo: false
-                  })
+                    noAlgo: false,
+                });
                 break;
             case "meanshift":
-                meanShift()
+                meanShift();
                 this.setState({
-                    noAlgo: false
-                  })
+                    noAlgo: false,
+                });
                 break;
             case "dbscan":
-                  dbScan()
-                  this.setState({
-                    noAlgo: false
-                  })
-                  break;
+                dbScan();
+                this.setState({
+                    noAlgo: false,
+                });
+                break;
             default:
                 this.setState({
-                    noAlgo: true
-                  })
+                    noAlgo: true,
+                });
         }
-    }
+    };
     public render() {
         return (
-        <ThemeProvider theme = {theme}>
-        <AppBar elevation={0} className="appbar" color="primary" style={{ color: 'white', minHeight: '80px' }}>
-        <Typography 
-            component="div"
-            mt = {2}
-            ml = {2}
-            ><Box sx={{ fontSize: 'h5.fontSize', fontWeight: 'bold' }}>Clustering Visualizer</Box></Typography>
-        <Toolbar>
-        <Grid container justifyContent="center" alignItems="center" style={{ height: '100%', position: 'relative', top: '8px' }}>
-        <Grid container justifyContent="center" alignItems="center" item xs={12} md={10} xl={9}>
-                <AlgorithmMenu {...this.props.global}/>
-                <Box
-                ml="20px">
-                <RandomMenu
-                {...this.props.global}/></Box>
-                <Box
-                ml="20px">{this.props.global.started ? (this.props.global.paused ?
-                    <Button
-                    variant='contained' color="secondary" onClick={this.props.resume}>Resume</Button>
-                    : 
-                    <Button color="secondary" variant='contained' onClick={this.props.pause}>Pause</Button>):(<Button
-                        variant='contained'
-                        color="secondary"
-                        onClick={this.handleStart}
-                        disabled = {this.props.global.started}>
-                            {this.state.noAlgo && !this.props.global.algorithm ?
-                         "Select an Algorithm" : this.props.global.points.length <= 2 ? "Plot points": "Visualize !"}
-                </Button>)}</Box>
-                {this.props.global.algorithm === "kmeans" ? <span>Set k clusters
-                <Box
-                ml="20px"><CentroidSlider/></Box>
-                </span> : null}
-               {this.props.global.algorithm === "meanshift" ? <span>Set Window Size
-               <Box
-                ml="20px"><WindowSizeSlider/></Box>
-                </span> : null}
-              {this.props.global.algorithm === "dbscan" ? <span>Set Epsilon
-                <Box
-                ml="20px"><EpsilonSlider/></Box>
-                </span> : null}
-                {this.props.global.algorithm === "dbscan" ? <span>Set Min Points
-                <Box
-                ml="20px"><MinPointsSlider/></Box>
-                </span> : null}
-                <Box ml="20px"><SpeedController/></Box>
-                <Box
-                ml="20px"><Button
-                variant='contained'
-                disabled = {this.props.global.started}
-                onClick = {this.props.reset}
-                >RESET
-                </Button></Box>
-                <Box
-                ml="20px"><DarkModeButton/></Box>   
-        </Grid>
-        </Grid>
-        </Toolbar>
-    </AppBar>
-    </ThemeProvider>
-    )
-}
+            <ThemeProvider theme={theme}>
+                <AppBar
+                    elevation={0}
+                    className="appbar"
+                    color="primary"
+                    style={{ color: "white", minHeight: "80px" }}
+                >
+                    <Typography component="div" mt={2} ml={2}>
+                        <Box
+                            sx={{ fontSize: "h5.fontSize", fontWeight: "bold" }}
+                        >
+                            Clustering Visualizer
+                        </Box>
+                    </Typography>
+                    <Toolbar>
+                        <Grid
+                            container
+                            justifyContent="center"
+                            alignItems="center"
+                            style={{
+                                height: "100%",
+                                position: "relative",
+                                top: "8px",
+                            }}
+                        >
+                            <Grid
+                                container
+                                justifyContent="center"
+                                alignItems="center"
+                                item
+                                xs={12}
+                                md={10}
+                                xl={9}
+                            >
+                                <AlgorithmMenu {...this.props.global} />
+                                <Box ml="20px">
+                                    <RandomMenu {...this.props.global} />
+                                </Box>
+                                <Box ml="20px">
+                                    {this.props.global.started ? (
+                                        this.props.global.paused ? (
+                                            <Button
+                                                variant="contained"
+                                                color="secondary"
+                                                onClick={this.props.resume}
+                                            >
+                                                Resume
+                                            </Button>
+                                        ) : (
+                                            <Button
+                                                color="secondary"
+                                                variant="contained"
+                                                onClick={this.props.pause}
+                                            >
+                                                Pause
+                                            </Button>
+                                        )
+                                    ) : (
+                                        <Button
+                                            variant="contained"
+                                            color="secondary"
+                                            onClick={this.handleStart}
+                                            disabled={this.props.global.started}
+                                        >
+                                            {this.state.noAlgo &&
+                                            !this.props.global.algorithm
+                                                ? "Select an Algorithm"
+                                                : this.props.global.points
+                                                      .length <= 2
+                                                ? "Plot points"
+                                                : "Visualize !"}
+                                        </Button>
+                                    )}
+                                </Box>
+                                {this.props.global.algorithm === "kmeans" ? (
+                                    <span>
+                                        Set k clusters
+                                        <Box ml="20px">
+                                            <CentroidSlider />
+                                        </Box>
+                                    </span>
+                                ) : null}
+                                {this.props.global.algorithm === "meanshift" ? (
+                                    <span>
+                                        Set Window Size
+                                        <Box ml="20px">
+                                            <WindowSizeSlider />
+                                        </Box>
+                                    </span>
+                                ) : null}
+                                {this.props.global.algorithm === "dbscan" ? (
+                                    <span>
+                                        Set Epsilon
+                                        <Box ml="20px">
+                                            <EpsilonSlider />
+                                        </Box>
+                                    </span>
+                                ) : null}
+                                {this.props.global.algorithm === "dbscan" ? (
+                                    <span>
+                                        Set Min Points
+                                        <Box ml="20px">
+                                            <MinPointsSlider />
+                                        </Box>
+                                    </span>
+                                ) : null}
+                                <Box ml="20px">
+                                    <SpeedController />
+                                </Box>
+                                <Box ml="20px">
+                                    <Button
+                                        variant="contained"
+                                        disabled={this.props.global.started}
+                                        onClick={this.props.reset}
+                                    >
+                                        RESET
+                                    </Button>
+                                </Box>
+                                <Box ml="20px">
+                                    <DarkModeButton />
+                                </Box>
+                            </Grid>
+                        </Grid>
+                    </Toolbar>
+                </AppBar>
+            </ThemeProvider>
+        );
+    }
 }
 
-export default connector(NavBar)
+export default connector(NavBar);
